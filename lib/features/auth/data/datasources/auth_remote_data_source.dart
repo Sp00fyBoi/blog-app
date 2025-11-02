7abcb6cbdc726a6b55e34340238ea72dc7a94138
Thanseer Hishak <thanseer.hishak30@gmail.com>
2025-11-02 22:47:32 +0530

    Initial commit

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,50 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../../core/error/exceptions.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+
+  @override
+  Future<String> loginWithEmailPassword({required String email, required String password}) async {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({required String name, required String email, required String password}) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {
+          'name' : name,
+        },
+      );
+
+      if (response.user == null) {
+        throw const ServerException('Failed to create user');
+      }
+
+      return response.user!.id;
+
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+  
+}
